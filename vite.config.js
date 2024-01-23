3fc6baa6177db5278e57908b1903a6513016c169
yuch3n <dustchen1126@gmail.com>
2024-01-23 16:56:44 +0800

    add bootstrap and sass

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/my-portfolio-2024/",
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
 });
